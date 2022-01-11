5fced60263f2b4830691c2b85386da95023b28e7
Tizian Graeber <82grti1bif@hft-stuttgart.de>
2022-01-11 10:15:01 +0100

    REEEEEEEEv2

--- src/debug.hpp
+++ src/debug.hpp
@@ -1,8 +1,8 @@
-#ifndef MCSEEDS_DEBUG_H
-#define MCSEEDS_DEBUG_H
+#ifndef MCSEEDS_DEBUG_HPP
+#define MCSEEDS_DEBUG_HPP
 
 #include <cstdio>
 
 #define dbg_printf(fmt, ...) do { printf("[%s:%d] " fmt, __FILE__, __LINE__, __VA_ARGS__); } while (0);
 
-#endif //MCSEEDS_DEBUG_H
+#endif //MCSEEDS_DEBUG_HPP
